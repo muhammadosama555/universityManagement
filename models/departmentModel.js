74fb53e5c85d9ba2af6910396ab8e759dd2ce3b6
outray-waleed <121039329+outray-waleed@users.noreply.github.com>
2023-05-30 17:48:53 +0500

    Update departmentModel.js

--- models/departmentModel.js
+++ models/departmentModel.js
@@ -10,12 +10,21 @@ module.exports = (sequelize, DataTypes) => {
       name: {
         type: DataTypes.STRING,
         allowNull: false
+      }, 
+    head: { 
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Professors', // It's the table name that the `head` object references
+        key: 'id'
       }
+    } 
 
       
     });
     
-  
+  Department.belongsTo(Professor, { foreignKey: 'head' });
+
 return Department;
   };
   
